dc07aa842ad673f72412c719851f82de29d04494
MikeFreno <michaelt.freno@gmail.com>
2023-02-28 16:06:49 -0500

    update to channel handling

--- src/functions/default.ts
+++ src/functions/default.ts
@@ -12,53 +12,69 @@ type payload = {
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   let payload: payload;
+  const connectionId = event.requestContext.connectionId;
   if (event.body) {
     payload = JSON.parse(event.body);
     const senderID = payload.senderID;
     const channelID = parseInt(payload.channelID);
-    const message = payload.message;
+    let message;
+    // message request
+    if (payload.message) {
+      message = payload.message;
+      const comment = await prisma.comment.create({
+        data: {
+          message: message,
+          channelID: channelID,
+          userId: senderID,
+        },
+      });
 
-    const comment = await prisma.comment.create({
-      data: {
-        message: message,
-        channelID: channelID,
-        userId: senderID,
-      },
-    });
+      const connections = await prisma.wSConnection.findMany({
+        where: {
+          channelID: channelID,
+        },
+      });
+      const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: "2018-11-29",
+        endpoint: `https://${event.requestContext.domainName}/${event.requestContext.stage}`,
+      });
 
-    const connections = await prisma.wSConnection.findMany({
-      where: {
-        channelID: channelID,
-      },
-    });
-    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-      apiVersion: "2018-11-29",
-      endpoint: `https://${event.requestContext.domainName}/${event.requestContext.stage}`,
-    });
-
-    await Promise.all(
-      connections.map(async (connection) => {
-        try {
-          await apigwManagementApi
-            .postToConnection({
-              ConnectionId: connection.connectionID,
-              Data: JSON.stringify(comment),
-            })
-            .promise();
-        } catch (e) {
-          if (e.statusCode === 410) {
-            // If a connection is no longer available, delete it from the database.
-            await prisma.wSConnection.delete({
-              where: { connectionID: connection.connectionID },
-            });
-          } else {
-            throw e;
+      await Promise.all(
+        connections.map(async (connection) => {
+          try {
+            await apigwManagementApi
+              .postToConnection({
+                ConnectionId: connection.connectionID,
+                Data: JSON.stringify(comment),
+              })
+              .promise();
+          } catch (e) {
+            if (e.statusCode === 410) {
+              // If a connection is no longer available, delete it from the database.
+              await prisma.wSConnection.delete({
+                where: { connectionID: connection.connectionID },
+              });
+            } else {
+              throw e;
+            }
           }
-        }
-      })
-    );
-
-    return { statusCode: 200, body: "Message sent." };
+        })
+      );
+      return { statusCode: 200, body: "Message sent." };
+    }
+    //channel select request
+    else {
+      prisma.wSConnection.update({
+        where: {
+          connectionID: connectionId,
+        },
+        data: {
+          channelID: channelID,
+          userId: senderID,
+        },
+      });
+      return { statusCode: 200, body: "Connection update." };
+    }
   } else {
     return { statusCode: 400, body: "Invalid request." };
   }
